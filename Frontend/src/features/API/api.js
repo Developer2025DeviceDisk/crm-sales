e1877ae2c1ca0b85749cf81297dbb82ab0768693
sumit2004tiwari <sumit2004tiwari@gmail.com>
2026-09-17 18:09:25 +0530

    live frontend with real url

--- Frontend/src/features/API/api.js
+++ Frontend/src/features/API/api.js
@@ -10,7 +10,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "https://crm-sales-76l5.onrender.com",
+    baseUrl: import.meta.env.VITE_API_URL || "https://crm-sales-76l5.onrender.com/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
     credentials: "include",
   }),
 
